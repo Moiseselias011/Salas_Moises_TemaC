68559aa0c8ff12deecb10de1289740c249ba641b
Moises011 <eriannbel@gmail.com>
2026-06-13 15:29:56 -0300

    Create JefeBrigada.java
    commit jefedebrigada

---
+++ Salas_Moises_TemaC/src/salas_moises_temac/JefeBrigada.java
@@ -0,0 +1,14 @@
+
+package salas_moises_temac;
+
+
+public class JefeBrigada extends Trabajador {
+
+    public JefeBrigada() {
+    }
+
+    public void informarReparacion() {
+        System.out.println("Reparacion informada");
+    }
+
+}

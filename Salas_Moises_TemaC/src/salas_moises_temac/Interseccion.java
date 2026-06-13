033f11f508ae8be340c139ecbb70bf287be9e633
Moises011 <eriannbel@gmail.com>
2026-06-13 15:03:04 -0300

    interseccion
    commit interseccion

---
+++ Salas_Moises_TemaC/src/salas_moises_temac/Interseccion.java
@@ -0,0 +1,23 @@
+
+package salas_moises_temac;
+
+
+public class Interseccion {
+    
+    private String calleA;
+    private String calleB;
+    private int altura;
+    private String barrio;
+
+    public Interseccion() {
+    }
+
+    public Interseccion(String calleA, String calleB, int altura, String barrio) {
+        this.calleA = calleA;
+        this.calleB = calleB;
+        this.altura = altura;
+        this.barrio = barrio;
+    }
+
+}
+

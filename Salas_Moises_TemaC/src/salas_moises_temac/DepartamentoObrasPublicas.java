0806162b0b178d6ef5e7557f21c3ac911cebd7bd
Moises011 <eriannbel@gmail.com>
2026-06-13 16:01:50 -0300

    commit
    commit

--- Salas_Moises_TemaC/src/salas_moises_temac/DepartamentoObrasPublicas.java
+++ Salas_Moises_TemaC/src/salas_moises_temac/DepartamentoObrasPublicas.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package salas_moises_temac;
 
-/**
- *
- * @author vanne
- */
+import java.util.ArrayList;
+
 public class DepartamentoObrasPublicas {
-    
+
+    private ArrayList<Ciudadano> ciudadanos;
+    private ArrayList<InformeRotura> informes;
+    private ArrayList<PedidoObra> pedidos;
+    private ArrayList<Brigada> brigadas;
+
+    public DepartamentoObrasPublicas() {
+        ciudadanos = new ArrayList<>();
+        informes = new ArrayList<>();
+        pedidos = new ArrayList<>();
+        brigadas = new ArrayList<>();
+    }
+
+    public void registrarCiudadano(Ciudadano c) {
+        ciudadanos.add(c);
+    }
+
+    public void informesPorCiudadano() {
+    }
+
+    public void informesSinPedidos() {
+    }
+
+    public void informeBrigadasOciosas() {
+    }
+
+    public void pedidosPorBrigada() {
+    }
+
+    public void bachesSinReparar() {
+    }
+
+    public void trabajadoresPorBrigada() {
+    }
+
 }
